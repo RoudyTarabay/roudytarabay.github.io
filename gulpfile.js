95267772cad3c5aaff1b5ae5e190597791d289c4
Roudy Tarabay <roudytarabay@gmail.com>
2017-01-27 20:13:32 +0200

    Putting my personal website instead

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp=require('gulp');
+gulp.task('autoprefixer', function () {
+    var postcss      = require('gulp-postcss');
+    var sourcemaps   = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+
+    return gulp.src('./css/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dest'));
+});
